b571a669567a7179d160df48d09f93af8f539f21
Flora Ting <ting.flora@gmail.com>
2021-09-13 20:12:22 +0000

    Add show_help_options function to explain how to use the calculator

--- calculator.py
+++ calculator.py
@@ -3,27 +3,30 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
 myops_1arg = {
-    "square":square,
-    "cube":cube
+    "square": square,
+    "cube": cube
 }
 
-
 myops_2args = {
-    "+":add,
-    "-":subtract,
-    "*":multiply,
-    "/":divide,
-    "pow":power,
-    "mod":mod
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "pow": power,
+    "mod": mod
 }
 
 
 def calculate(input_tokens):
-    """Checks for beginning operator, then calls corresponding arithmetic function"""
-    op = input_tokens[0]
+    """Return calculation after checking for operator at index 0 and calling the corresponding arithmetic function.
+    
+    INPUT: tokenized string --> ['pow', 3, 5]
+    OUTPUT: calculation --> 3 to the power of 5 --> 243
 
+    If this is not a valid expression, the function will return an error message.
+    """
+    op = input_tokens[0]
     if op in myops_1arg:
         oplist = myops_1arg
         operation = oplist[op]
@@ -47,43 +50,45 @@ def tokenize(input_string):     # 'pow 3 5' vs 'square 3'
             return 'error'
     return tokens
 
-"""
-GOAL: re-implement the REPL in calculator.py (from calculator-1 lab) from scratch.
-FUNCTIONS:
-    encapsulated environment that allows the user to:
-        LOOP: 
-            input individual arithmetic strings
-                check if input is the quit string
-                    close the program
-
-                # formatting
-                fx: tokenize the string into a list
-                
-                fx: identify which operation to perform
-                    EG: 'pow' ---> power()
-                    fx calls from arithemtic.py: evaluate the expression
-
-            quit the program with a specific string
-"""
 
 def main():
-    """Accepts user input in format of 'operator number number'
-    and performs calculation. Type q to quit.
+    """Opens up the REPL calculator interface within the Python interpreter.
+    
+    Accepts user input in string format of 'operator number number'
+    and performs calculation.
+    
+    Type q to quit.
     """
     while True:
-        print("What would you like to calculate? \
-        \nEG: 'pow 3 5' will calculate 3 to the power of 5")
+        print("What would you like to calculate?")
         # assumed to fit our format, might validate later
-        user_input = input("> ")          # 'pow 3 5'
-        tokens = tokenize(user_input)    # tokens = ['pow', 3, 5]
+        user_input = input("> ")        # 'pow 3 5'
+        tokens = tokenize(user_input)   # tokens = ['pow', 3, 5]
         if type(tokens) == str:
             print("This expression is invalid. Try again.")
             continue
             # check if tokens is valid (integer check)
-        elif tokens[0] == 'q' or tokens[0] == 'quit':
-            print("Okay, goodbye!") # exit statement
-            return  # exit the calculator
+        elif tokens[0] in ('q', 'quit'):
+            print("Okay, goodbye!")     # exit statement
+            return                      # exit the calculator
+        elif tokens[0] in ('?', 'help', 'options'):
+            show_help_options()
         else:
             print(calculate(tokens))
+
+
+def show_help_options():
+    """Show options via help menu."""
+    print("This is a prefix calculator. It will evaluate expressions \
+        \n in the format of 'operator operand1 operand2' and evaluate \
+        \n in the format of: operand1 operator operand2. \
+        \n\nEG: 'pow 3 5' will calculate 3 to the power of 5")
+    print("\nFunctions that take 1 argument include:")
+    for key, value in myops_1arg.items():
+        print("{:<10}".format(key), value.__name__)
+    print("\nFunctions that take 2 arguments include:")
+    for key, value in myops_2args.items():
+        print("{:<10}".format(key), value.__name__)
+    print("\nType 'q' or 'quit' to exit the program.\n")
 
 main()
